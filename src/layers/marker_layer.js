f8cf331c95627dc1c44d4032beee7a899274b8d0
fafa_a <fabienpuech1@gmail.com>
2022-04-24 23:41:17 +0200

    feat: add filtering cities

--- src/layers/marker_layer.js
+++ src/layers/marker_layer.js
@@ -1,3 +1,4 @@
+import L from "leaflet"
 import { useState } from "react"
 import { Marker, Popup } from "react-leaflet"
 import { defaultIcon } from "../icons/defaultIcon"
@@ -39,23 +40,38 @@ const PopupStatistics = ({ feature, setRadiusFilter }) => {
 }
 export const MarkerLayer = ({ data, setRadiusFilter, getRadiusFilter }) => {
   const radiusFilter = getRadiusFilter()
-  console.log(radiusFilter)
 
-  return data.features.map(feature => {
-    const { coordinates } = feature.geometry
-    return (
-      <Marker
-        key={String(coordinates)}
-        position={[coordinates[1], coordinates[0]]}
-        icon={defaultIcon}
-      >
-        <Popup>
-          <PopupStatistics
-            feature={feature}
-            setRadiusFilter={setRadiusFilter}
-          />
-        </Popup>
-      </Marker>
-    )
-  })
+  let centerPoint
+  if (radiusFilter) {
+    const { coordinates } = radiusFilter.feature.geometry
+    centerPoint = L.latLng(coordinates[1], coordinates[0])
+  }
+
+  return data.features
+    .filter(currentFeature => {
+      if (centerPoint) {
+        const { coordinates } = currentFeature.geometry
+        const currentPoint = L.latLng(coordinates[1], coordinates[0])
+        return centerPoint.distanceTo(currentPoint) / 1000 < radiusFilter.radius
+      } else {
+        return true
+      }
+    })
+    .map(feature => {
+      const { coordinates } = feature.geometry
+      return (
+        <Marker
+          key={String(coordinates)}
+          position={[coordinates[1], coordinates[0]]}
+          icon={defaultIcon}
+        >
+          <Popup>
+            <PopupStatistics
+              feature={feature}
+              setRadiusFilter={setRadiusFilter}
+            />
+          </Popup>
+        </Marker>
+      )
+    })
 }
