63c38f364440fb35c5ae2efbfa88220a2e9c71d4
fafa_a <fabienpuech1@gmail.com>
2022-04-24 23:10:55 +0200

    feat: add icon

--- src/layers/marker_layer.js
+++ src/layers/marker_layer.js
@@ -2,25 +2,25 @@ import React from "react"
 import { Marker, Popup } from "react-leaflet"
 import { defaultIcon } from "../icons/defaultIcon"
 import { Button, Card, InputNumber, Space } from "antd"
-
+import { FilterOutlined } from "@ant-design/icons"
 const PopupStatistics = ({ feature }) => {
   const { NAME, ADM0NAME, POP_MAX } = feature.properties
   return (
     <>
-      <Card type="inner" title="Name">
+      <Card type="inner" title="Name" style={{ marginTop: 16 }}>
         <b>{`${NAME}, ${ADM0NAME}`}</b>
       </Card>
-      <Card type="inner" title="Population">
+      <Card type="inner" title="Population" style={{ marginTop: 16 }}>
         <b>{`${POP_MAX}`}</b>
       </Card>
-      <Card type="inner" title="Radius Filter">
+      <Card type="inner" title="Radius Filter" style={{ marginTop: 16 }}>
         <Space>
           <InputNumber
             defaultValue={3000}
             min={0}
             onChange={e => console.log(e)}
           ></InputNumber>
-          <Button type="primary" shape="round">
+          <Button type="primary" shape="round" icon={<FilterOutlined />}>
             Filter by km
           </Button>
         </Space>
