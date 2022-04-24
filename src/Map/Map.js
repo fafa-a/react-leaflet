d1ca4cfce4db975b140a970b154a3b9d316bff1e
fafa_a <fabienpuech1@gmail.com>
2022-04-24 16:58:23 +0200

    feat: marker functional comp & import data geojson

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,6 +1,17 @@
 import React from "react"
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
 import { defaultIcon } from "../icons/defaultIcon"
+import { cities } from "../data/cities"
+
+const MarkerLayer = () => {
+  return (
+    <Marker position={[51.505, -0.09]} icon={defaultIcon}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  )
+}
 
 export const Map = () => {
   const position = [51.505, -0.09]
@@ -11,11 +22,7 @@ export const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position} icon={defaultIcon}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <MarkerLayer />
     </MapContainer>
   )
 }
