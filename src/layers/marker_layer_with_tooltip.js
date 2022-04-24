76da77a906ae4f2cf72f74fbae1da62d29f59438
fafa_a <fabienpuech1@gmail.com>
2022-04-24 18:09:15 +0200

    feat: add click event

--- src/layers/marker_layer_with_tooltip.js
+++ src/layers/marker_layer_with_tooltip.js
@@ -1,9 +1,9 @@
 import React from "react"
-import { Marker, Tooltip } from "react-leaflet"
-import { defaultIcon } from "../icons/defaultIcon"
+import { Marker, Tooltip, useMap } from "react-leaflet"
 import { mountainIcon } from "../icons/mountainIcon"
 
 export const MarkerLayerWithTooltip = ({ data }) => {
+  const leafletMap = useMap()
   return data.features.map(feature => {
     const { coordinates } = feature.geometry
     const { name, elevation, continent } = feature.properties
@@ -12,6 +12,9 @@ export const MarkerLayerWithTooltip = ({ data }) => {
         key={String(coordinates)}
         position={[coordinates[1], coordinates[0]]}
         icon={mountainIcon}
+        eventHandlers={{
+          click: e => leafletMap.panTo(e.latlng),
+        }}
       >
         <Tooltip>
           <h3>Mt.{name}</h3>
