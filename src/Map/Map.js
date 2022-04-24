ac1722c5357efa724416635ef95782654e0b87b0
fafa_a <fabienpuech1@gmail.com>
2022-04-24 17:12:19 +0200

    feat: center map

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -21,10 +21,8 @@ const MarkerLayer = ({ data }) => {
 }
 
 export const Map = () => {
-  const position = [51.505, -0.09]
-
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+    <MapContainer center={[0, 0]} zoom={1} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
