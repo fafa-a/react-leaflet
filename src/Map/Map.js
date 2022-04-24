8c8e46328e58f38decd2780896d9dcafeaf456aa
fafa_a <fabienpuech1@gmail.com>
2022-04-24 16:44:19 +0200

    feat: create marker & default icon

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,20 +1,21 @@
 import React from "react"
-import { MapContainer, TileLayer } from "react-leaflet"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import { defaultIcon } from "../icons/defaultIcon"
 
 export const Map = () => {
   const position = [51.505, -0.09]
 
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {/* <Marker position={position}>
+      <Marker position={position} icon={defaultIcon}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
-      </Marker> */}
+      </Marker>
     </MapContainer>
   )
 }
