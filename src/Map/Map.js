908f64e03da797506cf27630fd13c64f26c45273
fafa-a <fabienpuech1@gmail.com>
2022-04-25 15:47:01 +0200

    feat : add another base layer

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -28,6 +28,12 @@ export const Map = () => {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
         </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="ESRI World Imagery">
+          <TileLayer
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+          />
+        </LayersControl.BaseLayer>
         <MarkerLayer
           data={cities}
           setRadiusFilter={setRadiusFilter}
