472ecf68ac9c26aab913d21047cb0a642be941ea
fafa-a <fabienpuech1@gmail.com>
2022-05-18 17:00:44 +0200

    feat: add info_panel > map

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -9,11 +9,12 @@ import { FitBoundToDataControl } from "../controls/fit_data_to_bounds"
 import { ShowActiveFiltersControl } from "../controls/show_active_filters"
 import { MarkerLayerWithTooltipCluster } from "../layers/marker_layer_with_tooltip_cluster"
 import { MarkerLayerWithTooltipReproject } from "../layers/marker_layer_with_tooltip_reproject"
-
+import { InfoPanel } from "../components/info_panel"
 import { continents } from "../data/continents"
 import { cities } from "../data/cities"
 import { mountains } from "../data/highest-points"
 import { irishCities2157 } from "../data/irish_cities_2157"
+import { Line } from "react-chartjs-2"
 
 export const Map = () => {
   const [geoFilter, setGeoFilter] = useState(null)
@@ -22,7 +23,7 @@ export const Map = () => {
   const [radiusFilter, setRadiusFilter] = useState(null)
   const getRadiusFilter = () => radiusFilter
   const [asyncCities, setAsyncCities] = useState({ features: [] })
-  
+
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(
@@ -35,43 +36,46 @@ export const Map = () => {
   }, [])
 
   return (
-    <MapContainer center={[0, 0]} zoom={1} scrollWheelZoom={true}>
-      <LayersControl position="topright">
-        <LayersControl.BaseLayer checked name="OSM Strets">
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    <>
+      <MapContainer center={[0, 0]} zoom={1} scrollWheelZoom={true}>
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer checked name="OSM Strets">
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="ESRI World Imagery">
+            <TileLayer
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+              attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+            />
+          </LayersControl.BaseLayer>
+          <MarkerLayer
+            data={asyncCities}
+            setRadiusFilter={setRadiusFilter}
+            getRadiusFilter={getRadiusFilter}
+            getGeoFilter={getGeoFilter}
           />
-        </LayersControl.BaseLayer>
-        <LayersControl.BaseLayer name="ESRI World Imagery">
-          <TileLayer
-            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-            attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+          <MarkerLayerWithTooltip data={mountains} />
+          <MarkerLayerWithTooltipCluster data={cities} />
+          <MarkerLayerWithTooltipReproject data={irishCities2157} />
+          <RadiusFilteR
+            radiusFilter={radiusFilter}
+            setRadiusFilter={setRadiusFilter}
           />
-        </LayersControl.BaseLayer>
-        <MarkerLayer
-          data={asyncCities}
-          setRadiusFilter={setRadiusFilter}
-          getRadiusFilter={getRadiusFilter}
-          getGeoFilter={getGeoFilter}
+          <ContinentsPolygonLayer
+            data={continents}
+            setGeoFilter={setGeoFilter}
+            getGeoFilter={getGeoFilter}
+          />
+        </LayersControl>
+        <FitBoundToDataControl />
+        <ShowActiveFiltersControl
+          getFilters={() => ({ geoFilter, radiusFilter })}
         />
-        <MarkerLayerWithTooltip data={mountains} />
-        <MarkerLayerWithTooltipCluster data={cities} />
-        <MarkerLayerWithTooltipReproject data={irishCities2157} />
-        <RadiusFilteR
-          radiusFilter={radiusFilter}
-          setRadiusFilter={setRadiusFilter}
-        />
-        <ContinentsPolygonLayer
-          data={continents}
-          setGeoFilter={setGeoFilter}
-          getGeoFilter={getGeoFilter}
-        />
-      </LayersControl>
-      <FitBoundToDataControl />
-      <ShowActiveFiltersControl
-        getFilters={() => ({ geoFilter, radiusFilter })}
-      />
-    </MapContainer>
+      </MapContainer>
+      <InfoPanel />
+    </>
   )
 }
